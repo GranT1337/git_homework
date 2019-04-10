b07c8f0074d6eb2477ec4b072294d8758e8b8ea6
Ostanin Kirill <grant1337@pm.me>
2019-04-10 12:06:44 +0400

    rename sout in additional branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hey");
+        System.out.println("Hey, additional branch");
         System.out.println("Hello, new additional branch number two");
         System.out.println("String for additional branch");
         new Main().example("Ничего");

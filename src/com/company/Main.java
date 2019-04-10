addc04519a2943e37ada0280dd2a7d14591d1e57
Ostanin Kirill <grant1337@pm.me>
2019-04-10 11:24:23 +0400

    Commit for additional branch number two

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hey");
         System.out.println("String for additional branch");// write your code here
+        System.out.println("Hello, new additional branch number two");
     }
 }

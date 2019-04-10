bf5bffbe5e92fd55e221dae3268935ef13ad3813
Ostanin Kirill <grant1337@pm.me>
2019-04-10 11:19:17 +0400

    Commit for additional branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hey");// write your code here
+        System.out.println("Hey");
+        System.out.println("String for additional branch");// write your code here
     }
 }
